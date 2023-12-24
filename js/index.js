4bfdc0385b756966e1df90f06680cb6902b8fa90
malrjane <yana.ml02@mail.ru>
2023-12-24 17:53:02 +0400

    updated js included

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
-let mainArr = document.querySelectorAll('.menu_article');
-  
-          let arr = document.querySelectorAll('.menu_drinks_food');
-          Array.from(mainArr).forEach((btn) => {
-            btn.addEventListener('click', (event) => {
-              event.preventDefault();
-              Array.from(arr).forEach((e) => {e.classList.remove('hidden');})
-            });
-          });
+const buttons = document.querySelectorAll('.wrapperMy');
+const click = document.querySelectorAll('.menu_article');
+
+let arr = document.querySelectorAll('.menu_drinks_food');
+Array.from(buttons).forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    btn.nextElementSibling.classList.toggle('hidden');
+    btn.firstElementChild.classList.toggle('click');
+  })
+});
+
+
+$(document).ready(function() {
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    clidesToScroll: 1,
+    speed: 1000,
+    easing: 'linear',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true, 
+    touchThreshold: 10,    //
+  });
+});
